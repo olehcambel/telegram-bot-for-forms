05a69213b539575b66ebd53584ea45861fbaf031
olehcambel <nickcambel5@gmail.com>
2018-05-13 00:27:47 +0300

    first commit fix port

--- helpers/statServer.js
+++ helpers/statServer.js
@@ -2,8 +2,8 @@ const dbmanager = require('./dbmanager');
 // const http = require('http');
 
 const PORT = process.env.PORT || 3000
-// http.createServer((req, res) => {
-// }).
+http.createServer((req, res) => {
+}).
 listen(PORT, () => {
   console.log('Statistics server listening on: 3000'); // eslint-disable-line no-console
 });
