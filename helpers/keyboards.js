ff10f6fa47636e55c884d94b082facf7075c1fad
olehcambel <nickcambel5@gmail.com>
2018-05-14 18:35:53 +0300

    add languageInterface

--- helpers/keyboards.js
+++ helpers/keyboards.js
@@ -159,6 +159,8 @@ function sendTextInGroup(user) {
 module.exports = {
   mainMenuKeyboard,
   sendMainMenu,
+  setLanguageMenu,
+  setLanguageKeyboard,
   botInGroup,
   sendKeyboard,
   sendInline,
